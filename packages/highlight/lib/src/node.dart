670a80211dce72797b224c324ec79246aa60d1f0
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2021-10-03 16:19:45 +0200

    Update node.dart

--- packages/highlight/lib/src/node.dart
+++ packages/highlight/lib/src/node.dart
@@ -1,21 +1,7 @@
 class Node {
-  Node({
-    this.className,
-    this.value,
-    this.children,
-    this.noPrefix = false,
-    this.fillEmpty = false,
-  });
-
-  final String? className;
-  final String? value;
-  final List<Node>? children;
-  final bool noPrefix;
-  // Tells if the node is an empty space that has to be filled.
-  final bool fillEmpty;
-
-  @override
-  String toString() {
-    return 'Node(className: $className, value: $value, children: $children, noPrefix: $noPrefix)';
-  }
+  String? className;
+  String? value;
+  List<Node>? children;
+  bool noPrefix;
+  Node({this.className, this.value, this.children, this.noPrefix = false});
 }
